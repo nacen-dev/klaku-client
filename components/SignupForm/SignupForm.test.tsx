7ab8efe67e61cc32544416d99c8d5bb4a530eadd
Vincent <nacen.dev@gmail.com>
2022-06-07 13:23:33 +0800

    test: change username to email

--- components/SignupForm/SignupForm.test.tsx
+++ components/SignupForm/SignupForm.test.tsx
@@ -4,13 +4,13 @@ import { SignupForm } from "./SignupForm";
 describe("Signup Form", () => {
   const submit = jest.fn();
 
-  it("should have a username, first name, last name, password, confirm password fields, and signup button", () => {
+  it("should have a email, first name, last name, password, confirm password fields, and signup button", () => {
     render(<SignupForm submit={submit} />);
 
     const password = screen.getAllByLabelText(/password/i)[0];
     const confirmPassword = screen.getAllByLabelText(/password/i)[1];
 
-    expect(screen.getByLabelText(/username/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/email/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/first name/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/last name/i)).toBeInTheDocument();
     expect(password).toBeInTheDocument();
