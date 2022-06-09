86e59fba6e96087bee5b0bc6a858f37173ee9193
Vincent <nacen.dev@gmail.com>
2022-06-10 00:06:22 +0800

    feat: utilize useMutation hook to register user

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,11 +1,36 @@
 import type { NextPage } from "next";
-import { registerUser, signUpFormData } from "../axios/userApi";
+import { useState } from "react";
+import { useMutation } from "react-query";
+import { registerUser } from "../axios/userApi";
 import { SignupForm } from "../components/SignupForm/SignupForm";
 
 const Signup: NextPage = () => {
+  const [signUpMessage, setSignUpMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const { mutate } = useMutation(registerUser, {
+    onSuccess: (responseData) => {
+      console.log(responseData);
+
+      if (responseData.status === 200) {
+        setErrorMessage("");
+        setSignUpMessage(responseData.data.message);
+      } else {
+        setErrorMessage(responseData.data.error);
+        setSignUpMessage("");
+      }
+    },
+    onError: () => {
+      setErrorMessage("An error has occurred");
+    },
+  });
+
   return (
-    <div>
-      <SignupForm submit={registerUser} />
+    <div className="flex justify-center items-center h-[80%]">
+      <SignupForm
+        submit={mutate}
+        signUpMessage={signUpMessage}
+        errorMessage={errorMessage}
+      />
     </div>
   );
 };
