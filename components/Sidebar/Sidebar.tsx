1578c338ac6cad41ed664446effb1edbb1542d46
Vincent <nacen.dev@gmail.com>
2022-06-13 20:20:28 +0800

    style: add className prop, fix spacing

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,28 +1,29 @@
 import React from "react";
 import { Nav } from "../Nav/Nav";
-import { MdClose } from 'react-icons/md'
+import { MdClose } from "react-icons/md";
 
 interface Props {
   show: boolean;
   close: () => void;
+  className: string;
 }
 
-export const Sidebar = ({ show, close }: Props) => {
+export const Sidebar = ({ show, close, className }: Props) => {
   return (
     <div
       className={`${
         show ? "translate-x-0" : "translate-x-full"
-      } fixed w-[200px] h-full right-0 top-0 bg-black text-white transition-transform duration-300`}
+      } fixed w-[200px] h-full right-0 top-0 bg-black text-white transition-transform duration-300 ${className}`}
     >
       <aside className="p-6">
         <div className="mb-4 flex gap-4 items-center justify-between text-xl">
           <p>Menu</p>
-          <MdClose className="text-2xl cursor-pointer" onClick={close}/>
+          <MdClose className="text-2xl cursor-pointer" onClick={close} />
         </div>
         <Nav
           className="flex-col flex text-xl"
           isLoggedIn={false}
-          listClass="flex-col flex"
+          listClass="flex-col flex gap-2"
         />
       </aside>
     </div>
