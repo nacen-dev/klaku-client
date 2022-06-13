08ca55a4e1a80ec36312299aeec151bf43bd459b
Vincent <nacen.dev@gmail.com>
2022-06-13 21:30:46 +0800

    refactor: add more properties to interface

--- axios/productsApi.ts
+++ axios/productsApi.ts
@@ -1,14 +1,27 @@
 import axios from "axios";
 
-interface Products {
+interface IReview {
+  name: string;
+  comment: string;
+  rating: number;
+}
+
+interface IProducts {
+  _id: string;
   name: string;
   description: string;
-  _id: string
+  image: string;
+  size?: string;
+  color?: string;
+  category?: string;
+  rating: number;
+  stock: number;
+  reviews?: IReview[];
 }
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
 
-export const getAllProducts = (): Promise<Products[]> =>
+export const getAllProducts = (): Promise<IProducts[]> =>
   api.get("/products").then((res) => res.data);
