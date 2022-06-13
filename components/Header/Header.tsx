4847bcd7b5d45d658fab8c456132cac61e959c69
Vincent <nacen.dev@gmail.com>
2022-06-13 20:20:38 +0800

    feat: Header component

---
+++ components/Header/Header.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { Cart } from "../Cart/Cart";
+import { Hamburger } from "../Hamburger/Hamburger";
+import { Nav } from "../Nav/Nav";
+import { Sidebar } from "../Sidebar/Sidebar";
+
+interface Props {}
+
+export const Header = (props: Props) => {
+  const [show, setShow] = useState(false);
+
+  const toggleHamburger = () => {
+    setShow((prevState) => !prevState);
+  };
+
+  const closeMenu = () => {
+    setShow(false);
+  }
+
+
+  return (
+    <>
+      <div className="py-4 px-6 flex justify-between items-center bg-black text-white text-xl h-[10vh]">
+        <p>Klaku Logo</p>
+        <div className="flex justify-between gap-4">
+          <Nav isLoggedIn={false} className="md-max:hidden" listClass="flex gap-4"/>
+          <Hamburger click={toggleHamburger} className="md:hidden" />
+          <Cart />
+        </div>
+      </div>
+      <Sidebar show={show} close={closeMenu} className="md:hidden"/>
+    </>
+  );
+};
