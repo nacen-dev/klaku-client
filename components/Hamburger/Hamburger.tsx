dbd6dc40ef1ef32dd228a01e2f372fa5ee9ed35b
Vincent <nacen.dev@gmail.com>
2022-06-13 20:19:47 +0800

    style: add className prop

--- components/Hamburger/Hamburger.tsx
+++ components/Hamburger/Hamburger.tsx
@@ -4,19 +4,21 @@ interface Props {
   click: () => void;
   lineColorClass?: string;
   buttonBackgroundClass?: string;
+  className?: string;
 }
 
 export const Hamburger = ({
   click,
   lineColorClass,
   buttonBackgroundClass,
+  className
 }: Props) => {
   return (
     <button
       onClick={click}
       className={`w-8 h-7 ${
         buttonBackgroundClass ? buttonBackgroundClass : "bg-transparent"
-      } border-none flex justify-around flex-col cursor-pointer p-0`}
+      } border-none flex justify-around flex-col cursor-pointer p-0 ${className}`}
     >
       <div
         className={`w-8 h-[2px] ${
