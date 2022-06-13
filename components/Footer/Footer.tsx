1ae0b14f0bf4d2e51c0b2b571093ef09ea27d00e
Vincent <nacen.dev@gmail.com>
2022-06-13 20:47:36 +0800

    feat: footer

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {
+
+};
+
+export const Footer = (props: Props) => {
+  return (
+    <footer className="bg-slate-700 text-white p-4 text-center">
+      Copyright &copy; 2022 Klaku Clothing
+    </footer>
+  )
+};
