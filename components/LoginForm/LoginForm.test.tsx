7b91fe8100752a80b73210c2815f1c4d70cf2a24
Vincent <nacen.dev@gmail.com>
2022-06-18 15:54:45 +0800

    test: login is called with user's information

--- components/LoginForm/LoginForm.test.tsx
+++ components/LoginForm/LoginForm.test.tsx
@@ -1,21 +1,40 @@
-import { screen, render } from "@testing-library/react";
+import { screen, render, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { LoginForm } from "./LoginForm";
 
 describe("Login Form", () => {
   const login = jest.fn();
   it("has email, password field and login button", () => {
-    render(<LoginForm login={login}/>);
+    render(<LoginForm login={login} />);
 
     const emailField = screen.getByLabelText(/email/i);
     const passwordField = screen.getByLabelText(/password/i);
     const loginButton = screen.getByRole("button", { name: /login/i });
-  
+
     expect(emailField).toBeInTheDocument();
     expect(passwordField).toBeInTheDocument();
     expect(loginButton).toBeInTheDocument();
-    
   });
 
-  
+  it("should allow the user to login with their information", async () => {
+    render(<LoginForm login={login} />);
+
+    const email = screen.getByLabelText(/email/i);
+    const password = screen.getByLabelText(/password/i);
+    const loginButton = screen.getByRole("button", { name: /login/i });
+
+    const user = userEvent.setup();
+
+    await user.type(email, "nacen@test.com");
+    await user.type(password, "123456");
+    await user.click(loginButton);
+
+    await waitFor(() =>
+      expect(login).toHaveBeenCalledWith({
+        email: "nacen@test.com",
+        password: "123456",
+      })
+    );
+  });
 });
