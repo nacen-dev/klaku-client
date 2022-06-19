626dc0ca8c1fcba8aa0226253abdc3c9fd176403
Vincent <nacen.dev@gmail.com>
2022-06-19 20:11:28 +0800

    chore: remove unused dependency

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import { QueryClientProvider, QueryClient, Hydrate } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { useState } from "react";
 import { Layout } from "../components/Layout/Layout";
-import { StateProvider } from "../context/StateContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
