181cf3a0bd3d84c881ca49944097a450a50ba93c
Vincent <nacen.dev@gmail.com>
2022-06-20 00:07:53 +0800

    refactor: ICartItem product type to IProduct

--- state.ts
+++ state.ts
@@ -4,18 +4,33 @@ export interface IAuth {
   accessToken: string;
 }
 
-export interface ICartItem {
-  productId: string;
-  quantity: number;
+interface IReview {
+  name: string;
+  comment: string;
+  rating: number;
 }
 
-export interface ICart {
-  items: ICartItem[];
+export interface IProduct {
+  _id: string;
+  name: string;
+  description: string;
+  image: string;
+  size?: string;
+  color?: string;
+  category?: string;
+  rating: number;
+  stock: number;
+  reviews: IReview[];
+  price: number;
+}
+export interface ICartItem {
+  product: IProduct;
+  quantity: number;
 }
 
 interface IState {
   auth: IAuth;
-  cart: ICart[];
+  cart: ICartItem[];
 }
 
 export const { useGlobalState, getGlobalState, setGlobalState } =
@@ -23,3 +38,24 @@ export const { useGlobalState, getGlobalState, setGlobalState } =
     auth: { accessToken: "" },
     cart: [],
   });
+
+export const indexOfProductInCart = (cart: ICartItem[], productId: string) => {
+  for (let i = 0; i < cart.length; i++) {
+    console.log("product id in cart", cart[i].product._id);
+    if (cart[i].product._id === productId) return i;
+  }
+  return -1;
+};
+
+export const addToCart = (product: IProduct, quantity: number = 1) => {
+  const cart = getGlobalState("cart");
+  const setCart = (value: ICartItem[]) => setGlobalState("cart", value);
+  const index = indexOfProductInCart(cart, product._id);
+  const updatedCart = [...cart];
+  if (index < 0) {
+    updatedCart.push({ product, quantity });
+  } else {
+    updatedCart[index].quantity += quantity;
+  }
+  setCart(updatedCart);
+};
