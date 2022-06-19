3326198a0c7a4d8a42c351c7eed538fa618ef00c
Vincent <nacen.dev@gmail.com>
2022-06-19 19:45:45 +0800

    feat: add onNavItemClick prop

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -5,27 +5,34 @@ import { Logout } from "../Logout/Logout";
 
 interface Props extends React.HTMLAttributes<HTMLElement> {
   listClass?: string;
+  onNavItemClick?: () => void;
 }
 
-export const Nav = ({ className, listClass }: Props) => {
-  const [ auth, setAuth ] = useGlobalState("auth");
+export const Nav = ({ className, listClass, onNavItemClick }: Props) => {
+  const [auth, setAuth] = useGlobalState("auth");
 
   return (
     <nav className={className}>
       <ul className={`${listClass}`}>
         <li>
-          <Link href="/shop">Shop</Link>
+          <Link href="/shop">
+            <a onClick={onNavItemClick}> Shop</a>
+          </Link>
         </li>
         <li>
-          <Link href="/contact">Contact</Link>
+          <Link href="/contact">
+            <a onClick={onNavItemClick}>Contact</a>
+          </Link>
         </li>
         {auth.accessToken ? (
-          <li>
+          <li onClick={onNavItemClick}>
             <Logout />
           </li>
         ) : (
           <li>
-            <Link href="/login">Login</Link>
+            <Link href="/login">
+              <a onClick={onNavItemClick}>Login</a>
+            </Link>
           </li>
         )}
       </ul>
