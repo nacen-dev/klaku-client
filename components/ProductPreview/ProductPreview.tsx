07d55f480d3f70de4d6d765e871cbba3810049f5
Vincent <nacen.dev@gmail.com>
2022-06-20 00:09:03 +0800

    refactor: addToCart and change productPreview type

--- components/ProductPreview/ProductPreview.tsx
+++ components/ProductPreview/ProductPreview.tsx
@@ -1,31 +1,33 @@
 import Link from "next/link";
 import React from "react";
-
-export interface IProductPreview {
-  _id: string;
-  name: string;
-  price: number;
-  image: string;
-}
+import { addToCart, IProduct } from "../../state";
 
 interface Props {
-  productPreviewData: IProductPreview;
+  productPreviewData: IProduct;
 }
 
 export const ProductPreview = ({ productPreviewData: product }: Props) => {
   return (
-    <div className="h-[350px] w-full my-2">
+    <div className="w-full my-2 bg-white rounded-lg flex flex-col">
       <Link href={`/shop/products/${product._id}`}>
         <div
           style={{ backgroundImage: `url(${product.image})` }}
-          className="h-[90%] bg-center bg-cover cursor-pointer"
+          className="h-[300px] bg-center bg-cover cursor-pointer"
           role="img"
           aria-label={product.name}
         />
       </Link>
-      <div className="flex justify-between h-[10%] my-1">
-        <p>{product.name}</p>
-        <p>${product.price}</p>
+      <div className="px-4 py-2 flex-1 flex flex-col justify-between">
+        <div className="flex justify-between my-1">
+          <p>{product.name}</p>
+          <p>${product.price}</p>
+        </div>
+        <button
+          className="w-full bg-slate-700 text-white px-4 py-2 mb-2 hover:bg-sky-700"
+          onClick={() => addToCart(product)}
+        >
+          Add to Cart
+        </button>
       </div>
     </div>
   );
