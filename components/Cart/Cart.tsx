32e2bba49329bae06e02ade0c4048b6a54bf52ad
Vincent <nacen.dev@gmail.com>
2022-06-20 18:11:29 +0800

    feat: Cart component
    Add sidebar to open cart component, cart component gets data from global state.
    Show cart items inside sidebar.

--- components/Cart/Cart.tsx
+++ components/Cart/Cart.tsx
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsBag } from "react-icons/bs";
+import { Sidebar } from "../Sidebar/Sidebar";
+import { BsArrowLeft } from "react-icons/bs";
+import { useGlobalState } from "../../state";
+import { CartItems } from "./CartItems";
 
 interface Props {}
 
 export const Cart = (props: Props) => {
-  const cartItemCount = 0;
+  const [showCart, setShowCart] = useState(false);
+  const [cart, setCart] = useGlobalState("cart");
+
+  const openCart = () => setShowCart(true);
+
+  const closeCart = () => setShowCart(false);
+
   return (
-    <div className="flex cursor-pointer gap-3">
-      <p>Cart</p>
-      <BsBag className="text-2xl relative" />
-      <span className="bg-red-700 text-white rounded-xl px-1 text-xs flex items-center justify-center min-w-[22px] absolute right-3">
-        {cartItemCount}
-      </span>
-    </div>
+    <>
+      <div className="flex cursor-pointer gap-3" onClick={openCart}>
+        <p>Cart</p>
+        <BsBag className="text-2xl relative" />
+        <span className="bg-red-700 text-white rounded-xl px-1 text-xs flex items-center justify-center min-w-[22px] absolute right-3">
+          {cart.length}
+        </span>
+      </div>
+      <Sidebar
+        show={showCart}
+        close={closeCart}
+        className="max-w-[450px] !w-full"
+      >
+        <div className="p-4">
+          <div className="flex items-center gap-4 mb-8">
+            <BsArrowLeft
+              className="text-4xl cursor-pointer"
+              onClick={closeCart}
+            />
+            <p className="text-2xl mr-auto ml-auto">Your Cart</p>
+          </div>
+          <CartItems cart={cart} />
+        </div>
+      </Sidebar>
+    </>
   );
 };
