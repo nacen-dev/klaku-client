4ab981e2a3051e1bd01068659c9814d3c4155d7b
Vincent <nacen.dev@gmail.com>
2022-06-20 18:05:04 +0800

    feat: Redirect 404 to shop page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppContext, AppProps } from "next/app";
 import { QueryClientProvider, QueryClient, Hydrate } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { useState } from "react";
 import { Layout } from "../components/Layout/Layout";
 import { PersistLogin } from "../hoc/PersistLogin";
+import App from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
@@ -22,5 +23,18 @@ function MyApp({ Component, pageProps }: AppProps) {
     </QueryClientProvider>
   );
 }
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext);
+
+  if (appContext.ctx.res?.statusCode === 404) {
+    appContext.ctx.res.writeHead(302, { Location: "/shop" });
+    appContext.ctx.res.end();
+    return;
+  }
+
+  return { ...appProps };
+};
 
 export default MyApp;
