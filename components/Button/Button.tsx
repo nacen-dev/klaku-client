c5b448c7a9f63b0770aa74abf83ede3abfd8e1ca
Vincent <nacen.dev@gmail.com>
2022-06-20 18:24:06 +0800

    style: add cursor pointer

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -5,7 +5,7 @@ interface Props extends React.HTMLAttributes<HTMLButtonElement> {}
 export const Button = (props: Props) => {
   return (
     <button
-      className="px-4 py-2 bg-slate-700 hover:bg-sky-700 text-white rounded w-full"
+      className="px-4 py-2 bg-slate-700 hover:bg-sky-700 text-white rounded w-full cursor-pointer"
       {...props}
     >
       {props.children}
