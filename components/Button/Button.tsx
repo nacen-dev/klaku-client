3ec9e5b0d7539ab65bb6d58111495413bc12fe86
Vincent <nacen.dev@gmail.com>
2022-06-20 18:23:00 +0800

    feat: styled Button component

---
+++ components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface Props extends React.HTMLAttributes<HTMLButtonElement> {}
+
+export const Button = (props: Props) => {
+  return (
+    <button
+      className="px-4 py-2 bg-slate-700 hover:bg-sky-700 text-white rounded w-full"
+      {...props}
+    >
+      {props.children}
+    </button>
+  );
+};
