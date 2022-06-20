d20da95ce833db9a324ac7cc7e06b22c840f041a
Vincent <nacen.dev@gmail.com>
2022-06-20 18:46:34 +0800

    style: change grid to be responsive

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -33,7 +33,7 @@ const Shop = () => {
 
   return (
     <div className="p-4 bg-neutral-200">
-      <div className="h-full grid grid-cols-4 gap-4 gap-y-8">
+      <div className="h-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 gap-y-8">
         {data?.map((product) => (
           <ProductPreview key={product._id} productPreviewData={product} />
         ))}
