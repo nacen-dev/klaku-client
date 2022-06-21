c3de7360d69fd1d57c9380129fe936ff67e6c84d
Vincent <nacen.dev@gmail.com>
2022-06-21 22:08:14 +0800

    feat: add disabled prop

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,14 +2,18 @@ import React from "react";
 
 interface Props extends React.HTMLAttributes<HTMLButtonElement> {
   type?: "button" | "submit" | "reset";
+  disabled?: boolean;
 }
 
 export const Button = (props: Props) => {
   return (
     <button
       {...props}
-      className={`px-4 py-2 bg-slate-700 hover:bg-sky-700 text-white rounded w-full cursor-pointer ${props.className}`}
+      className={`px-4 py-2 bg-slate-700 hover:bg-sky-700 text-white rounded w-full cursor-pointer ${
+        props.className ? props.className : ""
+      }`}
       type={props.type ? props.type : "button"}
+      disabled={props.disabled ? true : false}
     >
       {props.children}
     </button>
