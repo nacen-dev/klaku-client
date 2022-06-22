1075e0788706d68b152357e20c0280f25aa6d9a2
Vincent <nacen.dev@gmail.com>
2022-06-22 23:05:55 +0800

    feat: Order Summary added

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -3,22 +3,31 @@ import { loadStripe, StripeElementsOptions } from "@stripe/stripe-js";
 import { NextPage } from "next";
 import React, { useEffect, useState } from "react";
 import { makePayment } from "../axios/axiosAPI";
+import { CartItems } from "../components/Cart/CartItems";
 import { CheckoutForm } from "../components/CheckoutForm/CheckoutForm";
-import { getGlobalState } from "../state";
+import { useGlobalState } from "../state";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY as string);
 
 const Checkout: NextPage = () => {
   const [clientSecret, setClientSecret] = useState("");
+  const [cart, setCart] = useGlobalState("cart");
+  const [shippingPrice, setShippingPrice] = useState(0);
+  const [subTotal, setSubTotal] = useState(0);
 
   useEffect(() => {
-    const cart = getGlobalState("cart");
-    const payment = async (c: typeof cart) => {
-      const data = await makePayment(c);
+    const productsData = cart.map((cartItem) => ({
+      productId: cartItem.product._id,
+      quantity: cartItem.quantity,
+    }));
+    const payment = async (products: typeof productsData) => {
+      const data = await makePayment(products);
       setClientSecret(data.clientSecret);
+      setShippingPrice(data.shippingPrice);
+      setSubTotal(data.subTotal);
     };
-    payment(cart);
-  }, []);
+    payment(productsData);
+  }, [cart]);
 
   const options: StripeElementsOptions = {
     clientSecret,
@@ -28,8 +37,26 @@ const Checkout: NextPage = () => {
   };
 
   return (
-    <div className="h-screen p-8 bg-neutral-200">
-      <div className="bg-white p-8 rounded shadow-md mt-[10%] max-w-[640px] mr-auto ml-auto w-full">
+    <div className="h-full md:h-screen p-[5%] w-90% bg-neutral-200 flex flex-col md:flex-row">
+      <div className="w-full md:w-1/2 h-100% px-8 py-4 bg-slate-700 text-white flex flex-col">
+        <h2 className="text-2xl my-4 text-center">Order Summary</h2>
+        <CartItems cart={cart} className="flex-1" />
+        <div className="mt-8 flex flex-col gap-1">
+          <div className="flex justify-between text-lg">
+            <span>Subtotal:</span>
+            <span>${subTotal}</span>
+          </div>
+          <div className="flex justify-between text-lg">
+            <span className="text-lg">Shipping Price: </span>
+            <span>${shippingPrice}</span>
+          </div>
+          <div className="flex justify-between">
+            <span className="text-xl">Total: </span>
+            <span className="text-2xl font-bold">${subTotal + shippingPrice}</span>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white px-8 py-4 w-full md:w-1/2 h-full">
         {clientSecret && (
           <Elements options={options} stripe={stripePromise} key={clientSecret}>
             <CheckoutForm />
