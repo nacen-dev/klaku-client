375d811a218177b741189dd82b67c407c53fabcf
Vincent <nacen.dev@gmail.com>
2022-06-24 04:20:32 +0800

    feat: add text to button for accessibility

--- components/Hamburger/Hamburger.tsx
+++ components/Hamburger/Hamburger.tsx
@@ -7,34 +7,36 @@ interface Props {
   className?: string;
 }
 
-export const Hamburger:FC<Props> = ({
+export const Hamburger: FC<Props> = ({
   click,
   lineColorClass,
   buttonBackgroundClass,
-  className
+  className,
 }) => {
   return (
-    <button
-      onClick={click}
-      className={`w-8 h-7 ${
-        buttonBackgroundClass ? buttonBackgroundClass : "bg-transparent"
-      } border-none flex justify-around flex-col cursor-pointer p-0 ${className}`}
-    >
+    <button onClick={click} className="flex gap-4">
+      <span>Menu</span>
       <div
-        className={`w-8 h-[2px] ${
-          lineColorClass ? lineColorClass : "bg-white"
-        }`}
-      ></div>
-      <div
-        className={`w-8 h-[2px] ${
-          lineColorClass ? lineColorClass : "bg-white"
-        }`}
-      ></div>
-      <div
-        className={`w-8 h-[2px] ${
-          lineColorClass ? lineColorClass : "bg-white"
-        }`}
-      ></div>
+        className={`w-8 h-7 ${
+          buttonBackgroundClass ? buttonBackgroundClass : "bg-transparent"
+        } border-none flex justify-around flex-col cursor-pointer p-0 ${className}`}
+      >
+        <div
+          className={`w-8 h-[2px] ${
+            lineColorClass ? lineColorClass : "bg-white"
+          }`}
+        />
+        <div
+          className={`w-8 h-[2px] ${
+            lineColorClass ? lineColorClass : "bg-white"
+          }`}
+        />
+        <div
+          className={`w-8 h-[2px] ${
+            lineColorClass ? lineColorClass : "bg-white"
+          }`}
+        />
+      </div>
     </button>
   );
 };
