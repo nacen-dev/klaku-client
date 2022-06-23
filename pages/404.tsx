59eaa94f64b4ac1defebd6c4aac66db31cbefea3
Vincent <nacen.dev@gmail.com>
2022-06-24 02:25:13 +0800

    feat: custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,21 @@
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+import { Loader } from "../components/Loader/Loader";
+
+type Props = {};
+
+const Custom404 = (props: Props) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/");
+  }, []);
+
+  return (
+    <div className="h-fit-content">
+      <Loader />
+    </div>
+  );
+};
+
+export default Custom404;
