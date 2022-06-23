4a3ce0932d4bdb8c18c13752e547a2ec06f9a84a
Vincent <nacen.dev@gmail.com>
2022-06-24 04:19:13 +0800

    refactor: add index for image priority

--- components/ProductPreview/ProductPreview.tsx
+++ components/ProductPreview/ProductPreview.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React, { FC } from "react";
 
@@ -6,18 +7,25 @@ import { Button } from "../Button/Button";
 
 interface Props {
   productPreviewData: IProduct;
+  index: number;
 }
 
-export const ProductPreview: FC<Props> = ({ productPreviewData: product }) => {
+export const ProductPreview: FC<Props> = ({
+  productPreviewData: product,
+  index,
+}) => {
   return (
     <div className="w-full my-2 bg-white rounded-lg flex flex-col">
       <Link href={`/shop/products/${product._id}`}>
-        <div
-          style={{ backgroundImage: `url(${product.image})` }}
-          className="h-[300px] bg-center bg-cover cursor-pointer"
-          role="img"
-          aria-label={product.name}
-        />
+        <a className="h-[300px] relative">
+          <Image
+            layout="fill"
+            className="cursor-pointer"
+            src={product.image}
+            alt={product.name}
+            priority={index < 3}
+          />
+        </a>
       </Link>
       <div className="px-4 py-2 flex-1 flex flex-col justify-between">
         <div className="flex text-xl justify-between my-1 text-slate-700 font-semibold">
--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -39,8 +39,8 @@ const Shop = () => {
   return (
     <div className="p-4 bg-neutral-200">
       <div className="h-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 gap-y-8">
-        {data?.map((product) => (
-          <ProductPreview key={product._id} productPreviewData={product} />
+        {data?.map((product, index) => (
+          <ProductPreview key={product._id} productPreviewData={product} index={index} />
         ))}
       </div>
     </div>
