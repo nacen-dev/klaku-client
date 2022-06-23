23aa87aa57cb48c5986f2930320c8f4f592931ab
Vincent <nacen.dev@gmail.com>
2022-06-24 04:18:33 +0800

    chore: remove unused depedency

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,10 @@
 import "../styles/globals.css";
-import type { AppContext, AppProps } from "next/app";
+import type {  AppProps } from "next/app";
 import { QueryClientProvider, QueryClient, Hydrate } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { useState } from "react";
 import { Layout } from "../components/Layout/Layout";
 import { PersistLogin } from "../hoc/PersistLogin";
-import App from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
